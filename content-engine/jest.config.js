2441d1c9210e8fb872a8435af663ecd05f59525f
Niloy <atiqisrak@gmail.com>
2024-09-07 13:38:01 +0600

    updated and cleared stuffs

--- content-engine/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: "jest-environment-jsdom", // Make sure this line points to the installed module
-  //   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-};
